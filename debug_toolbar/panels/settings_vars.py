f99718048f7c9e048a0b14ca05f252fdc17aa23e
bkonkle <brandon@brandonkonkle.com>
2011-01-27 14:40:21 -0600

    Only capture a subset of the available settings

--- debug_toolbar/panels/settings_vars.py
+++ debug_toolbar/panels/settings_vars.py
@@ -1,8 +1,17 @@
+import re
+
 from django.conf import settings
 from django.template.loader import render_to_string
 from django.views.debug import get_safe_settings
 from django.utils.translation import ugettext_lazy as _
 from debug_toolbar.panels import DebugPanel
+
+LOGGED_SETTINGS = re.compile(
+    'CACHE_BACKEND|CACHE_MIDDLEWARE_KEY_PREFIX|CACHE_MIDDLEWARE_SECONDS|' +
+    'DATABASES|DEBUG|DEBUG_LOGGING_CONFIG|DEBUG_TOOLBAR_CONFIG|' +
+    'DEBUG_TOOLBAR_PANELS|INSTALLED_APPS|INTERNAL_IPS|MIDDLEWARE_CLASSES|' +
+    'TEMPLATE_CONTEXT_PROCESSORS|TEMPLATE_DEBUG|USE_I18N|USE_L10N'
+)
 
 
 class SettingsVarsDebugPanel(DebugPanel):
@@ -31,6 +40,10 @@ class SettingsVarsDebugPanel(DebugPanel):
     def process_response(self, request, response):
         if getattr(settings, 'DEBUG_LOGGING_CONFIG', {}).get('ENABLED', False):
             # Logging is enabled, so log the settings
-            stats = {}
-            stats['settings'] = get_safe_settings()
-            request.debug_logging_stats.update(stats)
+            safe_settings = get_safe_settings()
+            log_settings = {}
+            for k, v in safe_settings.items():
+                if LOGGED_SETTINGS.search(k):
+                    log_settings[k] = v
+            
+            request.debug_logging_stats['settings'] = log_settings
