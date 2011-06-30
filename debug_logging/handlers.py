839d84e7d972b368f3b2c4d4c5e5356cb6f98be5
bkonkle <brandon@brandonkonkle.com>
2011-06-29 22:26:17 -0500

    Added a requirements file, and adjusted the handler to look for open test runs. Still very much WIP

--- debug_logging/handlers.py
+++ debug_logging/handlers.py
@@ -1,11 +1,22 @@
 import logging
 
-from debug_logging.models import DebugLogRecord
+from debug_logging.models import TestRun, DebugLogRecord
 
 
 class DBHandler(logging.Handler):
     
     def emit(self, record):
         if type(record.msg) is dict:
+            # Pull the project name, hostname, and revision out of the record
+            filters = {}
+            for key in ('project_name', 'hostname', 'revision'):
+                if record.msg.has_key(key):
+                    filters[key] = record.msg.pop(key)
+            
+            # Check for a test run meeting the criteria that hasn't ended yet
+            test_run = TestRun.objects.filter(end__isnull=True, **filters)
+            if test_run:
+                record.msg['test_run'] = test_run
+            
             instance = DebugLogRecord(**record.msg)
             instance.save()
