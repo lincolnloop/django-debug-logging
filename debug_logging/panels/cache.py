1a814b945fdfea4ee10a4cad3c30a57252ff7975
bkonkle <brandon@konkle.us>
2011-09-09 11:46:13 -0700

    Removed the now-unnecessary init overrides for the cache and sql panels

--- debug_logging/panels/cache.py
+++ debug_logging/panels/cache.py
@@ -6,25 +6,6 @@ from debug_toolbar.panels.cache import CacheDebugPanel, CacheStatTracker
 class CacheLoggingPanel(CacheDebugPanel):
     """Extends the Cache debug panel to enable logging."""
     
-    def __init__(self, *args, **kwargs):
-        """
-        This method is copied & pasted from SQLDebugPanel to overcome the issue
-        described here:
-        
-        https://github.com/django-debug-toolbar/django-debug-toolbar/pull/178
-        
-        Once this issue is closed and the pull request is merged, this __init__
-        method can be entirely removed.
-        """
-        self.context.update(kwargs.get('context', {}))
-        # This is hackish but to prevent threading issues is somewhat needed
-        if isinstance(cache.cache, CacheStatTracker):
-            cache.cache.reset()
-            self.cache = cache.cache
-        else:
-            self.cache = CacheStatTracker(cache.cache)
-            cache.cache = self.cache
-    
     def process_response(self, request, response):
         if getattr(settings, 'DEBUG_LOGGING_CONFIG', {}).get('ENABLED', False):
             # Logging is enabled, so log the cache data
--- debug_logging/panels/sql.py
+++ debug_logging/panels/sql.py
@@ -15,25 +15,6 @@ except ImportError:
 class SQLLoggingPanel(SQLDebugPanel):
     """Extends the SQL debug panel to enable logging."""
     
-    def __init__(self, *args, **kwargs):
-        """
-        This method is copied & pasted from SQLDebugPanel to overcome the issue
-        described here:
-        
-        https://github.com/django-debug-toolbar/django-debug-toolbar/pull/178
-        
-        Once this issue is closed and the pull request is merged, this __init__
-        method can be entirely removed.
-        """
-        self.context.update(kwargs.get('context', {}))
-        self._offset = dict((k, len(connections[k].queries)) for k in connections)
-        self._sql_time = 0
-        self._num_queries = 0
-        self._queries = []
-        self._databases = {}
-        self._transaction_status = {}
-        self._transaction_ids = {}
-    
     def process_response(self, request, response):
         if getattr(settings, 'DEBUG_LOGGING_CONFIG', {}).get('ENABLED', False):
             # Call the nav_subtitle method so that the query data is captured
